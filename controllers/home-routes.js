5c9b8eff2f4e12c724a3311f62f810cfe3cfcfcd
Chris Waters <floridarealty001@gmail.com>
2022-03-25 15:48:43 -0400

    updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,43 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
-
-   console.log('======================');
-  Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      [sequelize.literal('(SELECT * FROM post ')]
-     
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', ],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+res.render('homepage')
 });
 
 
